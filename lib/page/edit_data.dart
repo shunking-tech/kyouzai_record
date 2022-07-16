8351c589161d9937c63022663d3666669622fa7c
中村駿輝 <shnktech@gmail.com>
2022-07-16 18:33:43 +0900

    Textfieldを作って、入力した値を保存ボタンを押下時にconsoleに出力する

--- lib/page/edit_data.dart
+++ lib/page/edit_data.dart
@@ -8,14 +8,26 @@ class EditData extends StatefulWidget {
 }
 
 class _EditDataState extends State<EditData> {
+  TextEditingController textEditingController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("編集画面"),
       ),
-      body: Center(
-        child: Text("編集画面"),
+      body: Column(
+        children: [
+          TextField(
+            controller: textEditingController,
+          ),
+          ElevatedButton(
+            child: Text("保存"),
+            onPressed: () {
+              print(textEditingController.text);
+            },
+          ),
+        ],
       ),
     );
   }
