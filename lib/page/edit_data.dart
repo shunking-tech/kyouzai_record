5a0dc58c253a4965c6daeef538c6f80e4f34d65d
中村駿輝 <shnktech@gmail.com>
2022-07-18 16:20:18 +0900

    Sliderで数値選択できるようにする

--- lib/page/edit_data.dart
+++ lib/page/edit_data.dart
@@ -10,6 +10,7 @@ class EditData extends StatefulWidget {
 class _EditDataState extends State<EditData> {
   TextEditingController titleEditingController = TextEditingController();
   TextEditingController memoEditingController = TextEditingController();
+  double sliderValue = 0.0;
 
   @override
   Widget build(BuildContext context) {
@@ -32,6 +33,34 @@ class _EditDataState extends State<EditData> {
               labelText: 'メモ',
             ),
           ),
+          SizedBox(
+            height: 16.0,
+          ),
+          Align(
+            alignment: Alignment.centerLeft,
+            child: Text(
+              "数値",
+              style: TextStyle(fontSize: 16),
+            ),
+          ),
+          Slider(
+            label: sliderValue.toInt().toString(),
+            min: 0,
+            max: 100,
+            value: sliderValue,
+            activeColor: Colors.blue,
+            inactiveColor: Colors.grey,
+            divisions: 20,
+            onChanged: (value) {
+              setState(() {
+                sliderValue = value;
+              });
+            },
+          ),
+          Text(
+            sliderValue.toInt().toString(),
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          ),
           ElevatedButton(
             child: Text("保存"),
             onPressed: () {
@@ -39,6 +68,8 @@ class _EditDataState extends State<EditData> {
               print(titleEditingController.text);
               print("メモ");
               print(memoEditingController.text);
+              print("数値");
+              print(sliderValue);
             },
           ),
         ],
